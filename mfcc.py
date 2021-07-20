3d266e3548e39609eced80b33ee4d3b7e066548f
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-07-20 13:17:03 +0200

    spectral centroid added

--- mfcc.py
+++ mfcc.py
@@ -2,9 +2,10 @@ import librosa
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 import librosa.display
+import sklearn
 
 
-def waveplot(x, sr):
+def waveform(x, sr):
     plt.figure()
     librosa.display.waveplot(x, sr=sr)
     return plt.show()
@@ -27,12 +28,33 @@ def zero_crossing(x):
     return sum(zero_crossings)
 
 
+def spectral_centroid(x, sr):
+    spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
+    return spectral_centroids
+
+
+def sc_waveform(spectral_centroids):
+    spectral_centroids.shape
+    frames = range(len(spectral_centroids))
+    t = librosa.frames_to_time(frames)
+
+    def normalize(x, axis=0):
+        return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+    librosa.display.waveplot(x, sr=sr, alpha=0.4)
+    plt.plot(t, normalize(spectral_centroids), color='r')
+    return plt.show()
+
+
 if __name__ == '__main__':
     audio_path = 'data/wav/1-a_l.wav'
     x, sr = librosa.load(audio_path)
 
     ipd.Audio(audio_path)
-    waveplot = waveplot(x, sr)
+    waveform = waveform(x, sr)
     spectrogram = spectrogram(x, sr)
     zero_crossing = zero_crossing(x)
+    spectral_centroids = spectral_centroid(x, sr)
+    sc_waveform = sc_waveform(spectral_centroids)
+
 
