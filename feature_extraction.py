ee5063e904c89273b38a1f42e7348646b8e8d1b4
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-08-03 15:59:38 +0200

    refactoring

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,33 +1,7 @@
 import librosa
-import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
-import sklearn
 from numpy import trapz
-import statistics
-
-
-def audio(audio_path):
-    x, sr = librosa.load(audio_path)
-    return x, sr
-
-
-def waveform(x, sr):
-    plt.figure()
-    librosa.display.waveplot(x, sr=sr)
-    return plt.show()
-
-
-def spectrogram(x, sr):
-    x_fourier = librosa.stft(x)
-    x_db = librosa.amplitude_to_db(abs(x_fourier))
-    plt.figure()
-    plt.subplot(1, 2, 1)
-    librosa.display.specshow(x_db, sr=sr, x_axis='time', y_axis='hz')
-    plt.subplot(1, 2, 2)
-    librosa.display.specshow(x_db, sr=sr, x_axis='time', y_axis='log')
-    plt.colorbar()
-    return plt.show()
 
 
 def zero_crossing(x):
@@ -36,37 +10,11 @@ def zero_crossing(x):
 
 
 def spectral_centroid(x, sr):
-    spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
-    return spectral_centroids
-
-
-def sc_waveform(spectral_centroids):
-    frames = range(len(spectral_centroids))
-    t = librosa.frames_to_time(frames)
-
-    def normalize(x, axis=0):
-        return sklearn.preprocessing.minmax_scale(x, axis=axis)
-
-    librosa.display.waveplot(x, sr=sr, alpha=0.4)
-    plt.plot(t, normalize(spectral_centroids), color='r')
-    return plt.show()
+    return librosa.feature.spectral_centroid(x, sr=sr)[0]
 
 
 def spectral_rolloff(x, sr):
-    spectral_rolloff = librosa.feature.spectral_rolloff(x, sr=sr)[0]
-    return spectral_rolloff
-
-
-def sr_waveform(spectral_rolloff, spectral_centroids):
-    frames = range(len(spectral_centroids))
-    t = librosa.frames_to_time(frames)
-
-    def normalize(x, axis=0):
-        return sklearn.preprocessing.minmax_scale(x, axis=axis)
-
-    librosa.display.waveplot(x, sr=sr, alpha=0.4)
-    plt.plot(t, normalize(spectral_rolloff), color='r')
-    return plt.show()
+    return librosa.feature.spectral_rolloff(x, sr=sr)[0]
 
 
 def mfcc(x, sr):
@@ -75,52 +23,49 @@ def mfcc(x, sr):
     return mfccs
 
 
-def mfccs(mfccs, sr):
-    return librosa.display.specshow(mfccs, sr=sr, x_axis='time')
+# def mfccs(mfccs, sr):
+#     return librosa.display.specshow(mfccs, sr=sr, x_axis='time')
 
 
-def auc(array):
+def get_auc(array):
     auc = trapz(array, dx=5)
     return auc
 
 
 def mean(array):
-    mean = statistics.mean(array)
-    return mean
+    return np.mean(array)
 
 
 def std(array):
-    std = np.std(array)
-    return std
+    return np.std(array)
 
 
 def percentile(array, p):
-    percentile = np.percentile(array, p)
-    return percentile
+    return np.percentile(array, p)
 
 
 def max_ptp_value(array):
-    max = np.max(array)
-    min = np.min(array)
-    value = abs(max-min)
+    max_value = np.max(array)
+    min_value = np.min(array)
+    value = abs(max_value-min_value)
     return value
 
 
 if __name__ == '__main__':
-    x, sr = audio('data/wav/1-a_h.wav')
+    x, sr = librosa.load('data/wav/1-a_h.wav')
 
-    waveform = waveform(x, sr)
-    spectrogram = spectrogram(x, sr)
+    # waveform = waveform(x, sr)
+    # spectrogram = spectrogram(x, sr)
     zero_crossing = zero_crossing(x)
     spectral_centroids = spectral_centroid(x, sr)
-    sc_waveform = sc_waveform(spectral_centroids)
+    # sc_waveform = sc_waveform(spectral_centroids)
     spectral_rolloff = spectral_rolloff(x, sr)
-    sr_waveform = sr_waveform(spectral_rolloff, spectral_centroids)
+    # sr_waveform = sr_waveform(spectral_rolloff, spectral_centroids)
     mfccs = mfcc(x, sr)
     # mfccs_daria = mfccs(mfccs, sr)
-    auc_centroids = auc(spectral_centroids)
+    auc_centroids = get_auc(spectral_centroids)
     print("AUC centroids: ", auc_centroids)
-    auc_rolloff = auc(spectral_rolloff)
+    auc_rolloff = get_auc(spectral_rolloff)
     print("AUC rolloff: ", auc_rolloff)
     mean_centroids = mean(spectral_centroids)
     print("Mean centroids: ", mean_centroids)
