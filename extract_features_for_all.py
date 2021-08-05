dea40e8b2aa6321a52795a71d06ff55fb81b9192
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-08-05 13:50:40 +0200

    Save to csv file added

--- extract_features_for_all.py
+++ extract_features_for_all.py
@@ -4,6 +4,7 @@ import librosa
 from os.path import isfile, join
 import pandas as pd
 import numpy as np
+import tqdm
 
 
 def feature_extraction(audio_path):
@@ -21,7 +22,7 @@ def feature_extraction(audio_path):
     rolloff_ptp_value = fe.max_ptp_value(spectral_rolloff)
     signal_mfcc = fe.mfcc(x, sr)
     signal_mfcc_shape = signal_mfcc.shape
-    signal_mfcc_mean = fe.mfcc_mean(signal_mfcc)
+    signal_mfcc_mean = fe.mfcc_means(signal_mfcc)
     return '', signal_zero_crossings, centroid_auc, rolloff_auc, centroid_mean, rolloff_mean, centroid_std,\
            rolloff_std, centroid_ptp_value, rolloff_ptp_value, signal_mfcc_mean, signal_mfcc_shape
 
@@ -31,9 +32,9 @@ if __name__ == '__main__':
     root_unhealthy = 'data/unhealthy'
     files_healthy = [join(root_healthy, file) for file in listdir(root_healthy) if isfile(join(root_healthy, file))]
     columns = ['ID', 'Zero crossings', 'Centroid AUC', 'Rolloff AUC', 'Centroid mean', 'Rolloff mean', 'Centroid STD',
-              'Rolloff STD', 'Centroid p-t-p value', 'Rolloff p-t-p value', 'MFCC mean', "MFCC shape", 'Label']
+              'Rolloff STD', 'Centroid p-t-p value', 'Rolloff p-t-p value', 'MFCC means', "MFCC shape", 'Label']
     df = pd.DataFrame(columns=columns)
-    for file in files_healthy:
+    for file in tqdm.tqdm(files_healthy):
         feats = feature_extraction(file)
         label = 'healthy'
         feats = list(feats)
@@ -43,7 +44,7 @@ if __name__ == '__main__':
 
     files_unhealthy = [join(root_unhealthy, file) for file in listdir(root_unhealthy) if isfile(join(root_unhealthy, file))]
 
-    for file in files_unhealthy:
+    for file in tqdm.tqdm(files_unhealthy):
         feats = feature_extraction(file)
         label = 'unhealthy'
         feats = list(feats)
@@ -51,4 +52,4 @@ if __name__ == '__main__':
         df2 = pd.DataFrame([feats], columns=columns)
         df = df.append(df2)
 
-print()
+    df.to_csv(join('data', 'features.csv'), index=False)
