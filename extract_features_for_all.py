85c974648e6b60e046a9af00179a8fe788165813
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-08-23 13:38:54 +0200

    new features added

--- extract_features_for_all.py
+++ extract_features_for_all.py
@@ -9,7 +9,7 @@ import tqdm
 
 def feature_extraction(audio_path):
     x, sr = librosa.load(audio_path)
-    features = np.zeros((49,))
+    features = np.zeros((75,))
     features[0] = audio_path.split("\\")[1].split("-")[0]
     features[1] = fe.zero_crossing(x)
     spectral_centroid = fe.spectral_centroid(x, sr)
@@ -24,8 +24,11 @@ def feature_extraction(audio_path):
     features[9] = fe.max_ptp_value(spectral_rolloff)
     signal_mfcc = fe.mfcc(x, sr)
     features[10:23] = np.mean(signal_mfcc, axis=1)
-    features[23:36] = fe.mfcc_delta(signal_mfcc[:, 1])
-    features[36:49] = fe.mfcc_delta2(signal_mfcc[:, 1])
+    features[23:36] = fe.mfcc_delta(signal_mfcc)
+    features[36:49] = fe.mfcc_delta2(signal_mfcc)
+
+    features[49:62] = fe.mfcc_max_min(signal_mfcc[:, 6:-6])
+    features[62:75] = fe.mfcc_std(signal_mfcc[:, 6:-6])
     return features
 
 
@@ -33,13 +36,15 @@ if __name__ == '__main__':
     root_healthy = 'data/healthy'
     root_unhealthy = 'data/unhealthy'
 
-    means = ['MFCC mean ' + str(x) for x in range(1, 14)]
+    means = [f'MFCC mean {x}' for x in range(1, 14)]
     deltas = ['MFCC delta ' + str(x) for x in range(1, 14)]
-    deltas2 = ['MFCC delta 2  ' + str(x) for x in range(1, 14)]
+    deltas2 = ['MFCC delta 2 ' + str(x) for x in range(1, 14)]
+    max_min = ['MFCC max_min ' + str(x) for x in range(1, 14)]
+    std = ['MFCC std ' + str(x) for x in range(1, 14)]
 
     files_healthy = [join(root_healthy, file) for file in listdir(root_healthy) if isfile(join(root_healthy, file))]
     columns = ['ID', 'Zero crossings', 'Centroid AUC', 'Rolloff AUC', 'Centroid mean', 'Rolloff mean', 'Centroid STD',
-              'Rolloff STD', 'Centroid p-t-p value', 'Rolloff p-t-p value'] + means + deltas + deltas2 + ['Label']
+              'Rolloff STD', 'Centroid p-t-p value', 'Rolloff p-t-p value'] + means + deltas + deltas2 + max_min + std + ['Label']
     df = pd.DataFrame(columns=columns)
     for file in tqdm.tqdm(files_healthy):
         feats = feature_extraction(file)
