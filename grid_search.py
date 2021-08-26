badf1d1f072508914cd1628414b2ace4e531bcb8
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-08-26 12:44:02 +0200

    grid search for LightGBM added

--- grid_search.py
+++ grid_search.py
@@ -8,15 +8,18 @@ df = pd.read_csv('data/features.csv')
 
 # grid_params = {'C': [10, 5, 30], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'gamma': ['auto', 'scale']}
 
-grid_params = {'n_estimators': [50, 100, 150], 'max_depth': [10, 20, 30],
-          'min_samples_split': [2, 5, 10, 20], 'min_samples_leaf': [2, 5, 10, 20],
-          'max_samples': [100, 200, 300]}
+# grid_params = {'n_estimators': [50, 100, 150], 'max_depth': [10, 20, 30],
+#           'min_samples_split': [2, 5, 10, 20], 'min_samples_leaf': [2, 5, 10, 20],
+#           'max_samples': [100, 200, 300]}
+
+grid_params = {'num_iterations': [5, 10, 20, 50], 'num_leaves': [5, 10, 20, 50],
+               'objective': ['regression', 'regression_l1', 'huber', 'fair', 'poisson', 'quantile', 'mape', 'gamma', 'tweedie', 'binary', 'cross_entropy', 'cross_entropy_lambda',
+                             'lambdarank', 'rank_xendcg', 'multiclass', 'multiclassova']}
 
 best_score = 0
 
 for params in tqdm.tqdm(itertools.product(*grid_params.values())):
-    params = {'n_estimators': params[0], 'max_depth': params[1], 'min_samples_split': params[2],
-              'min_samples_leaf': params[3], 'max_samples': params[4]}
+    params = {'num_iterations': params[0], 'num_leaves': params[1], 'tree_learner': params[2], 'objective': params[3]}
     # params = {'C': params[0], 'kernel': params[1], 'gamma': params[2]}
     test_result, train_result, train_x, model = train_model(df, params)
     if test_result > best_score:
